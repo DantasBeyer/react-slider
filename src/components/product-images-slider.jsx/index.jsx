84f570c935f3016d1c5078eb867040a422cd89b6
Dantas Beyer <dantasbeyer@gotmail.com>
2023-03-13 14:39:49 +0100

    thumb created

--- src/components/product-images-slider.jsx/index.jsx
+++ src/components/product-images-slider.jsx/index.jsx
@@ -15,11 +15,31 @@ function ProductImagesSlider (props) {
         navigation={true}
         modules={[Navigation,Thumbs]}
         grabCursos={true}
+        className="product-images-slider"
       >
         {
           props.images.map ((item,index) => 
             <SwiperSlide hey={index}>
               <img src={item} alt ="Slider Images" />
+            </SwiperSlide>
+          )
+        }
+      </Swiper>
+      <Swiper
+      /* colocar para centralizado 
+      debaixo do quadro de imagens */
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        modules={[Navigation,Thumbs]}
+        className="product-images-slider-thumbs"
+      >
+        {
+          props.images.map ((item,index) => 
+            <SwiperSlide hey={index}>
+              <div className="product-images-slider-thumbs-wrapper">
+                <img src={item} alt ="Slider Images" />
+              </div>
             </SwiperSlide>
           )
         }
